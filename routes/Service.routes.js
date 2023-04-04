66e4fa61683dcd6739be49159bee557546e93877
toseef847 <toseefhasan@gmail.com>
2023-04-05 01:31:55 +0500

    added services apis

--- routes/Service.routes.js
+++ routes/Service.routes.js
@@ -9,6 +9,6 @@ const upload = require("../middlewares/upload");
 const router = express.Router();
 
 router.post("/add", auth, checkAccess, upload.single("image"), addService);
-router.post("/get-all", getAllServices);
+router.get("/get-all", getAllServices);
 
 module.exports = router;
